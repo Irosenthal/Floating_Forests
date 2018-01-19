4f259b4edf4d7f7afdd7f3171b48e3d07e408958
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2018-01-19 10:37:09 -0500

    made load classifications dynamic to load any new classification file

--- scripts/visualization/load_prep_classifications.R
+++ scripts/visualization/load_prep_classifications.R
@@ -8,16 +8,29 @@ library(raster)
 library(rgeos)
 library(lubridate)
 
-#read in spatialPolygonsDataFrame (shapefile)
-#classifications <- readOGR("../../data/output/consensus_shapefiles/ff_polys_proj.sqlite", "ff_consensus", stringsAsFactors=FALSE) #slower
-classifications <- readRDS("../../data/output/consensus_shapefiles/ff_consensus_polys_zone_10.rds")
 
-#add time information
-classifications <- classifications %>% 
-  mutate(scene_timestamp = parse_date_time(scene_timestamp, orders="ymd"),
-         quarter = quarter(scene_timestamp, with_year=TRUE, fiscal_start=11))
+get_classifications <- function(filename = "ff_consensus_polys_zone_10.rds",
+                                datadir = "../../data/output/consensus_shapefiles/",
+                                return = "df"){
 
-#convert to data frame
-classifications_df <- classifications %>% tidy
-classifications@data$id <- sapply(classifications@polygons, function(x) x@ID)
-classifications_df <- left_join(classifications_df, classifications@data)
+  #read in spatialPolygonsDataFrame (shapefile)
+  #classifications <- readOGR("../../data/output/consensus_shapefiles/ff_polys_proj.sqlite", "ff_consensus", stringsAsFactors=FALSE) #slower
+  classifications <- readRDS(paste0(datadir, filename))
+
+  
+  #add time information
+  classifications <- classifications %>% 
+    mutate(scene_timestamp = parse_date_time(scene_timestamp, orders="ymd"),
+           quarter = quarter(scene_timestamp, with_year=TRUE, fiscal_start=11))
+  
+  if(return_value == "spdf") return(classifications)
+  
+  #convert to data frame
+  classifications_df <- classifications %>% tidy
+  classifications@data$id <- sapply(classifications@polygons, function(x) x@ID)
+  classifications_df <- left_join(classifications_df, classifications@data)
+  
+  if(return_value == "df") return(classifications_df)
+  
+  list(spdf = classifications, df = classifications_df)
+}
