5af6260282e7c0fdd26ae0790baa9f802305b0c0
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2019-01-16 13:51:44 -0500

    adding relaunch parsing

---
+++ scripts/relaunch_data_pipeline/data_pipeline/1_separate_data_from_workflows.R
@@ -0,0 +1,83 @@
+#' ##############################
+#' Separate classification, yes/no, 
+#' and other workflow data into separate 
+#' .Rds files
+#' 
+#' ##############################
+
+
+#load libraries
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(jsonlite)
+
+
+
+#read relaunch data
+ff_relaunch <- read_csv("../../../data/relaunch_data/raw_data/floating-forests-classifications.csv",
+                         col_types="icicicdcccccci") %>%
+   mutate(created_at = lubridate::parse_date_time(created_at, orders="ymdHMS"))
+ 
+#read subjects and deparse JSON 
+ff_relaunch_subjects <- read_csv("../../../data/relaunch_data/raw_data/floating-forests-subjects-2018-12-12.csv") %>% 
+  mutate(
+    metadata = map(metadata, ~fromJSON(.x) %>% as_tibble),
+    locations = map(locations, fromJSON)) %>%  
+  unnest(locations) %>%
+  mutate(locations = unlist(locations)) %>%  
+  unnest(metadata)
+
+
+# TO deal with data from when we worked on the beta of the site
+# ff_relaunch_beta <- ff_relaunch %>%
+#   filter(created_at < parse_date_time("12-12-2017", orders="mdy")) %>%
+#   filter(created_at > parse_date_time("04-18-2017", orders="mdy")) 
+
+# All data created after relaunch
+ff_relaunch <- ff_relaunch %>%
+  filter(created_at > parse_date_time("12-12-2017", orders="mdy"))
+
+##
+# split into different workflows and deparse the JSON
+##
+
+#helpful workflow/function
+flatten_json <- . %>%
+  fromJSON(.) %>% 
+  purrr::flatten() %>% 
+  map(~ifelse(is.null(.x), NA, .x)) %>%
+  as_tibble
+
+#lots of json error checking in here - oy
+#the metadata column just keeps changing...
+possibly_deparse <- possibly(function(x) x %>% fromJSON() %>% 
+                               purrr::flatten_df() %>% 
+                               mutate_all(as.character), 
+                             otherwise = tibble(no_metadata_info = TRUE))
+
+deparse_json <- . %>%
+  mutate(subject_data = map(subject_data, flatten_json),
+         metadata = map(metadata, possibly_deparse)) %>%
+  unnest(subject_data) %>%
+  select(-starts_with("V")) %>%
+  #deal with the constantly changing metadata JSON
+  mutate(metadata = map(metadata, ~setNames(.x, make.names(names(.x), unique = TRUE)))) %>%
+  mutate(metadata = map(metadata, ~select(.x, -starts_with("X")))) %>%
+  mutate(metadata = map(metadata, mutate_all, .funs = as.character)) %>%
+  unnest(metadata)
+
+
+ff_relaunch_classifications <- ff_relaunch %>%
+  filter(workflow_id == 2150)  %>% 
+  deparse_json()
+
+#swipe workflow only
+ff_relaunch_swipe <- ff_relaunch %>%
+  filter(workflow_id == 3246)  %>% 
+  deparse_json()
+
+#write data as rds to save space
+saveRDS(ff_relaunch_subjects, "../../../data/relaunch_data/level_0/ff_relaunch_subjects.rds")
+saveRDS(ff_relaunch_classifications, "../../../data/relaunch_data/level_0/floating_forests_classifications.rds")
+saveRDS(ff_relaunch_swipe, "../../../data/relaunch_data/level_0/floating_forests_swipe.rds")
---
+++ scripts/relaunch_data_pipeline/data_pipeline/Readme.md
@@ -0,0 +1,4 @@
+# Data Processing Pipeline for Floating Forests 2.0
+
+## Steps of processing Pipeline
+1. 
