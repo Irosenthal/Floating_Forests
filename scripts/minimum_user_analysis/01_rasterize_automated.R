e032cc7a105451be77589c5a0935f20c6c47f179
Isaac Rosenthal <irosenthal121@gmail.com>
2019-05-17 15:29:47 -0400

    drops all invalid geoms. next step is to have these recast as multipolygons instead of being dropped

--- scripts/minimum_user_analysis/01_rasterize_automated.R
+++ scripts/minimum_user_analysis/01_rasterize_automated.R
@@ -1,9 +1,3 @@
-#' ##############################
-#' Process raw FF relaunch data
-#' 
-#' ##############################
-
-
 #load libraries
 library(raster)
 library(sf)
@@ -14,23 +8,22 @@ library(purrr)
 library(furrr)
 library(lwgeom)
 library(stringr)
+library(tidyv)
 
 options(stringsAsFactors = FALSE)
 
 #setup multicore environment to use all cores available ####
 plan(multiprocess)
-#plan(sequential) #for testing
+plan(sequential) #for testing
 
 source("./_pipeline_functions.R")
 
-read_dir <-  "../../../data/relaunch_data/level_0/"
 write_dir <- "../../data/output/raw_data_pipeline/level_0/raster_tiles/"
 
 #read data
-sf_objects <- list.files(read_dir) %>%
-  str_subset("utm_zone") %>%
-  str_c(read_dir, .) %>%
-  map(readRDS)
+classifications <- readRDS("../../data/output/raw_data_pipeline/level_0/zone_11_classifications.rds")
+classifications$user_name <- as.factor(classifications$user_name) 
+
 
 ##
 # function to make a consensus raster tile ####
@@ -40,7 +33,9 @@ sf_objects <- list.files(read_dir) %>%
 ##
 
 #subj <- unique(sf_objects[[1]]$subject_ids)
-#one_subject <- sf_objects[[1]] %>% filter(subject_ids == subj[1])
+one_subject <- classifications %>% 
+  filter(subject_zooniverse_id == subject_zooniverse_id[1])
+#
 st_get_npts <- function(x){
   sapply(x, function(x) nrow(x[[1]]))
 }
@@ -80,60 +75,61 @@ st_remove_points <- function(x){
 }
 
 rasterize_one_subject <- function(one_subject, res = 10, write_out_tile = TRUE){
-  cat(str_c("Rasterizing ", classifications$subject_zooniverse_id, "...\n"))
-  
+  #cat(str_c("Rasterizing ", classifications$subject_zooniverse_id[[1]], "...\n"))
+  print(one_subject$subject_zooniverse_id[1])
   #make sure this isn't a blank tile
-  one_subject_nonempty <- one_subject %>%
-    dplyr::filter(!(sapply(one_subject$geometry, is_empty))) %>%
-    mutate(geometry = map(geometry, st_remove_points),
-            empt = map_lgl(geometry, st_is_empty)) %>%
-    dplyr::filter(!empt) %>%
-    dplyr::select(-empt)
+  #one_subject_nonempty <- one_subject %>%
+   # dplyr::filter(!(sapply(one_subject$geometry, is_empty))) %>%
+    #mutate(geometry = map(geometry, st_remove_points),
+ #           empt = map_lgl(geometry, st_is_empty)) %>%
+#    dplyr::filter(!empt) %>%
+ #   dplyr::select(-empt)
     
 #rasterize based on these nonempty SFS: get it working with all, then add ifelse to have it kick out a blank raster when empty
   
   
-  rast <- fasterize(one_subject_nonempty, #give it an SF
+  rast <- fasterize(one_subject, #give it an SF
                  raster(one_subject, nrows = 400, ncols = 364), #and a template for the raster
                  field = "user_name", #must be a factor (bug?)
                  fun = "count")
   
-  #write out the tile in the meanwhile
-  if(write_out_tile){
-    #supressing warnings for blank rasters
-    suppressWarnings(
-      out <- writeRaster(rast, str_c(write_dir, one_subject$subject_zooniverse_id[1], ".grd"), overwrite=TRUE)
-    )
+
+ writeRaster(rast, str_c(write_dir, one_subject$subject_zooniverse_id[1], ".grd"), overwrite=TRUE)
+    
   }
   
 
     return(rast)
 }
+#####################
+#testing 
 
-
-##
-# Pipeline:
-# for each data file
-# and for each subject, 
-# make a consensus raster tile
-##
-
-# testing code
-# 
-# # set.seed(5000)
-#  levs <- unique(sf_objects[[1]]$subject_ids) %>% sample(10, replace=FALSE)
-# #
-#  test <- sf_objects[[1]] %>%
-#   #filter(subject_ids %in% 15116674)
-#  filter(subject_ids %in% levs)
-# 
- test_rast <- map(split(classifications, classifications$subject_zooniverse_id), rasterize_one_subject)
-
-#see where it chokes
 bad_sub <- classifications %>%
-  filter(subject_zooniverse_id == "AKP0002pek")
+  filter(subject_zooniverse_id == "AKP000059a")
 
 
+classifications2 <- classifications[1:50,]
+test_split <-split(classifications2, classifications2$subject_zooniverse_id)
+test_rast <- map(test_split, rasterize_one_subject)
+
+
+#ok, it's breaking when they are polygons instead of multipolygons.
+#try dropping all polys
+
+classifications_filtered <- classifications %>%
+  filter(st_geometry_type(classifications$geometry) == "MULTIPOLYGON")
+
+
+
+######
+
+
+#run it
+test_rast <- map(split(classifications_filtered, classifications_filtered$subject_zooniverse_id), rasterize_one_subject)
+
+#check a few
+raster_check <- raster("../../data/output/raw_data_pipeline/level_0/raster_tiles/AKP000050o.grd")
+plot(raster_check)
 
 #filter out any tiles already done
 done_tiles <- list.files(write_dir) %>%
