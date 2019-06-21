645b968d289cf1684d51a93b81a94e4e5885137a
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2019-06-20 21:07:45 -0700

    updating user visualization

---
+++ scripts/relaunch_data_pipeline/visualization/user_survivorship.R
@@ -0,0 +1,61 @@
+#' ##############################
+#' Summarize user behavior over
+#' the course of the project
+#' ##############################
+
+
+#load libraries
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(tsibble)
+library(ggplot2)
+
+#the relevant directories
+read_dir <- "../../../data/relaunch_data/level_0/"
+write_fig_dir <-  "../../../figures/relaunch_viz/"
+
+ff_data <- readRDS(str_c(read_dir, "floating_forests_classifications.rds"))
+
+#make data into time aware tsibble
+ff_data_tsbl <- ff_data %>% 
+  select(classification_id, user_name, user_id, started_at) %>%
+  mutate(started_at =  parse_date_time(started_at, orders = "ymdHMS")) %>%
+  filter(!is.na(started_at)) %>%
+  filter(!str_detect(user_name, "not-logged-in")) %>%
+  as_tsibble(key = classification_id, index = started_at)
+
+#get daily user summary stats
+user_summary <- ff_data_tsbl %>%
+  
+  #first, get summary per user per week
+  group_by(user_name) %>%
+  index_by(year_week = as.Date(yearweek(started_at))) %>%
+  summarize(classifications = n()) %>%
+  ungroup() %>%
+  as_tibble() %>%
+  
+  #now let's clean it up to make some nice order to 
+  #users for the y-axis
+  group_by(user_name) %>%
+  mutate(min_date = min(year_week)) %>%
+  ungroup() %>%
+  arrange(user_name) %>%
+  mutate(ord = -1*rank(min_date, ties.method = "first")) %>%
+  group_by(user_name) %>%
+  mutate(ord = max(ord)) %>%
+  ungroup() %>%
+  mutate(ord = rank(ord)) %>%
+  arrange(year_week)
+
+#plot!
+ggplot(user_summary,
+       aes(x = ord, y = year_week, size = classifications)) +
+  geom_point(alpha = 0.5) +
+  coord_flip() +
+  theme_bw(base_size = 14) +
+  theme(axis.text.y = element_blank()) +
+  xlab("") + ylab("") +
+  guides(size = guide_legend("Classifications\nper week"))
+
+ggsave(str_c(write_fig_dir, "user_survivorship.jpg"))
